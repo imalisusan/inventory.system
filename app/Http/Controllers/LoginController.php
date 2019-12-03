9c49afb27539f4c5a30720d9d27077a72ee3fe50
Imali Susan <susanimali52@gmail.com>
2019-12-03 11:43:29 -0800

    Change.3

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+    protected $redirectTo ='/home';
+}
